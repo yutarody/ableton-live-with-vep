0b4c855471e4db473955c49cb43007999ae75bac
yutarody <yutaro.ver05+github@gmail.com>
2016-06-05 16:25:03 +0900

    change project dir search from log file

--- live.with.VEP.sh
+++ live.with.VEP.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 liveAlive=`ps -ef | grep "MacOS/Live" | grep -v grep | wc -l`
 viennaAlive=`ps -ef | grep "Vienna Ensemble Pro" | grep -v grep | wc -l`
+curpj=`cat "$HOME/Library/Preferences/Ableton/Live 9.6.1/Log.txt" | grep -i project | grep -o '/V.*Project' | sed -e s/\%20/\ /`
+#als=`find "$curpj" -depth 1 -name "*.als"`
+vep=`find "$curpj" -name "*.mframe64"`
 
 if [ $liveAlive = 1 ] && [ $viennaAlive = 0 ]; then
     open '/Applications/Vienna Ensemble Pro/Vienna Ensemble Pro Server (64-bit).app'
+    open "$vep"
 fi
